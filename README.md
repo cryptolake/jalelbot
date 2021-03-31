87ee6fe205e67725ebb158c534b7a70e72a0a582
crypt <dhiadah@gmail.com>
2021-03-31 01:11:43 +0100

    reintalize

---
+++ README.md
@@ -0,0 +1,16 @@
+# jalelbot
+It's jalel brick what else you want here?
+
+you need three files to for this bot:
+
+ `jalel`: random phrases.
+
+ `key`: put the discord api key here.
+
+ `ball`: jalel has the abilily to answer yes/no/maybe questions like the classic 8ball so put the diffrent reponses in this file.
+
+You also need 2 directories of sound files because YES JALEL CAN TALK:
+
+`allahu`: because jalel is very muslim i put some muslim songs and stuff here.
+
+`voices`: here you put clips of jalel talking or whatever the fuck you want idc.
---
+++ jalelbot.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import discord
+from random import random,randint
+from os import listdir
+
+jalel = open('./jalel').readlines()
+ball = open('./ball').readlines()
+st = listdir('./voices/')
+key = open('key').readline()
+allahu = listdir('./allahu/')
+client = discord.Client()
+global jaleli
+jaleli = "jalel"
+global tkalem
+tkalem = "tkalem"
+global allah
+allah = "rab"
+
+def quote():
+    return random.choice(jalel)
+
+def magicball():
+    return random.choice(ball)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        global counter
+        counter = 0
+        if message.author == self.user:
+            return
+        if "?" in message.content and jaleli in message.content.lower() and counter <= 5:
+            if "kadeh" in message.content:
+                await message.channel.send("ahawa kadeh: " + str(randint(0, 10)))
+            else:
+                await message.channel.send(magicball())
+            counter = counter + 1
+        elif jaleli in message.content.lower():
+            global voice
+            global channel
+            if tkalem in message.content.lower():
+                try:
+                    channel = message.author.voice.channel
+                    voice = await channel.connect()
+                    voice.play(source=discord.FFmpegPCMAudio(
+                        './st/' + random.choice(st)))
+                    await message.channel.send('hani jeyek')
+                except:
+                    voice.stop()
+                    voice.play(source=discord.FFmpegPCMAudio(
+                        './st/' + random.choice(st)))
+                    await message.channel.send('hani jeyek')
+            elif allah in message.content.lower():
+                try:
+                    voice.stop()
+                    voice.play(source=discord.FFmpegPCMAudio(
+                        './allahu/'+random.choice(allahu)))
+                except:
+                    channel = message.author.voice.channel
+                    voice = await channel.connect()
+                    voice.stop()
+                    voice.play(source=discord.FFmpegPCMAudio(
+                        './allahu/'+random.choice(allahu)))
+                    await message.channel.send('haw jayek rabek')
+
+            else:
+                await message.channel.send(quote())
+                await voice.disconnect()
+                counter = 0
+
+
+bot = MyClient()
+bot.run(key)
